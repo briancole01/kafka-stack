cca4ad9ddf49eb995a6f609f595f83dadbc4499c
brian cole <brian.cole@austrac.gov.au>
2017-05-09 18:59:39 +1000

    upgrade kafka version, add kafka properties, add more kafka brokers to the stack

--- kafka/run.sh
+++ kafka/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-
-sed "s/zookeeper.connect=.*/zookeeper.connect=${ZOOKEEPER_SERVERS}/g" -i /opt/kafka/config/server.properties
-exec /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties
+exec /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties \
+  --override zookeeper.connect=${ZOOKEEPER_SERVERS} \
+  --override broker.id=${BROKER_ID}   
