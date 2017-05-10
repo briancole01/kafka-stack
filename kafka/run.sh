00b28886259ec2f0e8c1c4b91c83be6934f37110
brian <brian@briansurface3>
2017-05-10 16:34:50 +1000

    add test for env variables not being set

--- kafka/run.sh
+++ kafka/run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+if [ !"$BROKER_ID" ] ; then
+  export BROKER_ID=1
+fi
+if [ !"$ZOOKEEPER_SERVERS" ] ; then
+  export ZOOKEEPER_SERVERS=zookeeper:2181
+fi
 exec /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties \
   --override zookeeper.connect=${ZOOKEEPER_SERVERS} \
-  --override broker.id=${BROKER_ID}   
+  --override broker.id=${BROKER_ID}  
