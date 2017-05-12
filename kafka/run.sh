21ec3b19f21363627aabab829d6b0b9b68fec7ec
brian cole <brian.cole@austrac.gov.au>
2017-05-12 14:08:41 +1000

    fix env variable name

--- kafka/run.sh
+++ kafka/run.sh
@@ -2,7 +2,7 @@
 if [[ -z "$BROKER_ID" ]] ; then
   export BROKER_ID=1
 fi
-if [[ -z "$ZOOKEEPER_ID" ]] ; then
+if [[ -z "$ZOOKEEPER_SERVERS" ]] ; then
   export ZOOKEEPER_SERVERS=localhost:2181
 fi
 exec /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties \
